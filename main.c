173dedf15926a0eeea336637b508d433ae92499a
Masterchef365 <duncan.freeman1@gmail.com>
2018-11-24 15:28:30 -0800

    replace fail() with perror()

--- main.c
+++ main.c
@@ -14,8 +14,6 @@
 
 #include <linux/joystick.h>
 
-void fail(char* where);
-
 int main (int argc, char** argv) {
 	/* Warn user of incorrect usage */
 	if (argc < 3) {
@@ -32,15 +30,15 @@ int main (int argc, char** argv) {
 			if ((joystick_fd = open(dir_buf, O_RDONLY)) != -1) break;
 		}
 	}
-	if (joystick_fd < 0) fail("during joystick acquisition");
+	if (joystick_fd < 0) perror("during joystick acquisition");
 
 	/* Create socket */
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) fail("on socket creation");
+	if (sockfd < 0) perror("on socket creation");
 
 	/* Acquire host IP */
 	struct hostent *server = gethostbyname(argv[1]);
-	if (!server) fail("on getting server ip");
+	if (!server) perror("on getting server ip");
 
 	/* Create server output struct and connect */
 	struct sockaddr_in serv_addr;
@@ -49,7 +47,7 @@ int main (int argc, char** argv) {
 	serv_addr.sin_port = htons(atoi(argv[2]));
 	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
 	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-		fail ("when connecting to server");
+		perror ("when connecting to server");
 
 	/* Buffer for serialized messages */
 	const int buf_len = 64;
@@ -78,17 +76,11 @@ int main (int argc, char** argv) {
 				break;
 		}
 		if (write(sockfd, message_buf, buf_len) < buf_len)
-			fail("on socket write");
+			perror("on socket write");
 	}
-	fail("on joystick read");
+	perror("on joystick read");
 
 	close(sockfd);
 	close(joystick_fd);
 
-}
-
-/* Check error message and quit */
-void fail(char* where) {
-	fprintf(stderr, "\e[31mError\e[m %s: %s\n", where, strerror(errno));
-	exit(EXIT_FAILURE);
 }
